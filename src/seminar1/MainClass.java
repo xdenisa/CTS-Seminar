4a2003dd1a4b9d08916f05744d994dd21c47198c
xdenisa <denisaiordache09@yahoo.com>
2021-02-25 19:01:23 +0200

    Adaugare clase

--- src/seminar1/MainClass.java
+++ src/seminar1/MainClass.java
@@ -4,9 +4,11 @@ public class MainClass {
     public static void main(String[] args) {
         Zoo zoo=new Zoo();
         Giraffe giraffe=new Giraffe("Girafa");
-        Zebra zebra=new Zebra("Zebra");
+        Zebra zebra1=new Zebra("Zebra1");
+        Zebra zebra2=new Zebra("Zebra2",5);
         zoo.AdaugaAnimal(giraffe);
-        zoo.AdaugaAnimal(zebra);
+        zoo.AdaugaAnimal(zebra1);
+        zoo.AdaugaAnimal(zebra2);
         zoo.FeedAnimals();
     }
 }
--- src/seminar1/Zebra.java
+++ src/seminar1/Zebra.java
@@ -1,7 +1,22 @@
 package seminar1;
 
 public class Zebra extends  Animal{
+    private int varsta;
     public Zebra(String name) {
         super(name);
+        varsta=1;
+    }
+
+    public int getVarsta() {
+        return varsta;
+    }
+
+    public void setVarsta(int varsta) {
+        this.varsta = varsta;
+    }
+
+    public Zebra(String name, int varsta) {
+        super(name);
+        this.varsta = varsta;
     }
 }
